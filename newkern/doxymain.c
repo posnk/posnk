1a6f6fe3fdf08325e87e7e878ef23c76b2b358b1
peterbjornx <peterbosc@gmail.com>
2014-07-13 20:24:09 +0200

    Added features page to website

--- newkern/doxymain.c
+++ newkern/doxymain.c
@@ -5,7 +5,7 @@
  * This is the website for posnk, an simple operating system written by Peter Bosch
  *
  * \ref fs_device FS driver API documentation\n
- * \ref vfs.c VFS API documentation 
+ * \ref vfs VFS documentation\n
  *
  */
 
@@ -15,7 +15,9 @@
  * \li \subpage large
  *
  */
+
 //-----------------------------------------------------------
+
 /** \page small Small CD image
  * <a href="downloads/pos_small_july10_14.iso"> Download here (26MB)</a>
  *
@@ -32,6 +34,7 @@
  * \li signal test program
  * \li multiuser support (ctrl+F[n] switches to vt[n]) 
  */
+
 //-----------------------------------------------------------
 /** \page large Large CD image
  * <a href="https://www.sendspace.com/file/x5w9tb"> Download here (150MB)</a>
@@ -51,4 +54,31 @@
  * \li klanges nyancat sources and build script (run bash ./make.sh)
  * \li a copy of George Orwell's 1984 (/root/1984.txt)
  *
+ */
+
+//-----------------------------------------------------------
+/** \page features Features
+ *
+ * \section now Current features:
+ *
+ * \li Partially UNIX-compatible file API
+ * \li Initial ramdisk loaded from tar files passed as modules
+ * \li Multitasking
+ * \li Signals
+ * \li Partial old signal API
+ * \li Ext2 filesystem (read-only)
+ * \li Partial UNIX timekeeping API implementation
+ * \li Memory mapped files
+ * \li Multiple virtual consoles
+ * \li UNIX TTY api
+ * \li User/group based permissions
+ * \li Post-mortem debugger hook
+ *
+ * \section future Planned features:
+ * \li mmap API
+ * \li In-kernel gdb server to ease debugging multitasking
+ * \li Ext2 write support
+ * \li Shared memory
+ * \li VGA Framebuffer driver  
+ */
  */
