34e76f1412496b7dc2e7270c9f7e4327434b1233
peterbjornx <peterbosc@gmail.com>
2015-03-04 19:13:39 +0100

    Added CHAINRET variant for SVFUNCs

--- newkern/include/crt/sys/error.h
+++ newkern/include/crt/sys/error.h
@@ -14,9 +14,14 @@
 #ifndef __SYS_ERROR_H__
 #define __SYS_ERROR_H__
 
+typedef int errno_t;
+
 #define RETURN_VALUE(TyPe)	TyPe* _retval
-#define RETURN(VaLuE)		do { *_retval = (VaLuE); return ESUCCESS; } while (0);
-#define THROW(StAtUs, VaLuE)	do { *_retval = (VaLuE); return <StAtUs>; } while (0);
-#define CHAINRET(FuNc, ...)	do { return FuNc(__VA_ARGS__, _retval); } while (0);
+#define RETURN(VaLuE)		do { *_retval = (VaLuE); return ESUCCESS; } while (0)
+#define THROW(StAtUs, VaLuE)	do { *_retval = (VaLuE); return StAtUs; } while (0)
+#define CHAINRET(FuNc, ...)	do { return FuNc(__VA_ARGS__, _retval); } while (0)
+#define CHAINRETV(FuNc, ...)	do { return FuNc(__VA_ARGS__); } while (0)
+#define SFUNC(TyPe, NaMe, ...)  errno_t NaMe(__VA_ARGS__, RETURN_VALUE(TyPe))
+#define SVFUNC(NaMe, ...)  errno_t NaMe(__VA_ARGS__)
 
 #endif
