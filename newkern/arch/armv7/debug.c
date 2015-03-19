9c74b75dd6c8c7a0f32657a53741f0a7aa7b85b9
peterbjornx <peterbosc@gmail.com>
2015-03-19 20:24:06 +0000

    Stubbed out debug routines for ARM

---
+++ newkern/arch/armv7/debug.c
@@ -0,0 +1,20 @@
+/**
+ * @file arch/armv7/debug.c
+ * 
+ * Part of P-OS kernel.
+ *
+ * Written by Peter Bosch <peterbosc@gmail.com>
+ * 
+ * Changelog:
+ * \li 19-03-2015 - Created
+ */
+
+#include <stddef.h>
+
+void debug_dump_state ( void *state, size_t state_size )
+{
+}
+
+void debug_postmortem_hook( void *state, size_t state_size, void * instr_addr )
+{
+}
