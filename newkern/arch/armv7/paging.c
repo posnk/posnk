1d6541e9a920dfff7885d0927fb537748f55c7d3
peterbjornx <peterbosc@gmail.com>
2015-03-20 08:16:40 +0000

    Made paging init debugging a little less verbose

--- newkern/arch/armv7/paging.c
+++ newkern/arch/armv7/paging.c
@@ -119,7 +119,6 @@ void armv7_paging_init(armv7_bootargs_t *args)
 		km_va	  = args->ba_kmap[kmap_ctr].ba_kmap_va;
 		km_va_end = km_va + args->ba_kmap[kmap_ctr].ba_kmap_sz;
 		km_pa 	  = args->ba_kmap[kmap_ctr].ba_kmap_pa;
-		earlycon_printf("paging: mapping k: 0x%x [0x%x-0x%x]\n", km_pa,km_va,km_va_end);
 		for (; km_va < km_va_end; km_va += 4096) {
 			
 			l1_entry = level1_va->entries[ARMV7_TO_L1_IDX(km_va)];
@@ -156,7 +155,6 @@ void armv7_paging_init(armv7_bootargs_t *args)
 	}
 
 
-	earlycon_printf("paging: l1table address: 0x%x [0x%x]\n", level1_pa, level1_va->entries[ARMV7_TO_L1_IDX(0xC0000000)]);
 	armv7_mmu_set_dacr (0x55555555);
 
 	armv7_mmu_set_ttbr0((uint32_t)level1_pa);
@@ -166,7 +164,6 @@ void armv7_paging_init(armv7_bootargs_t *args)
 
 	armv7_special_l2 = (void *)ARMV7_PAGE_L2SPEC;
 	level1_va = (void *)ARMV7_INITIAL_L1;
-	earlycon_printf("paging: online , l1table address: 0x%x [0x%x]\n", level1_pa, level1_va->entries[ARMV7_TO_L1_IDX(0xC0000000)]);
 
 	paging_active_dir = &armv7_page_dir_initial;
 	paging_active_dir->content = level1_va;
