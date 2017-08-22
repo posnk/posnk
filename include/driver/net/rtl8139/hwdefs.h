5ff258e2938bdd26e19ea046a5bfebb4397d7b17
peterbjornx <peterbosc@gmail.com>
2017-08-22 11:08:14 +0000

    Start work on rtl8139 driver

---
+++ include/driver/net/rtl8139/hwdefs.h
@@ -0,0 +1,68 @@
+/*
+ * Part of P-OS kernel
+ * Created: 22-08-2017 by Peter Bosch
+ */
+
+#ifndef RTL8193_HWDEFS
+#define RTL8193_HWDEFS
+#include <stdint.h>
+
+#define RTL8139_REG_IDR(i)	(0x0000+i)
+#define RTL8139_REG_MAR(i)	(0x0008+i)
+#define RTL8139_REG_TSD(i)	(0x0010+i*0x4)
+#define RTL8139_REG_TSAD(i)	(0x0020+i*0x4)
+#define RTL8139_REG_RBSTART	(0x0030)
+#define RTL8139_REG_ERBCR	(0x0034)
+#define RTL8139_REG_ERSR	(0x0036)
+#define RTL8139_REG_CR		(0x0037)
+#define RTL8139_REG_CAPR	(0x0038)
+#define RTL8139_REG_CBR		(0x003A)
+#define RTL8139_REG_IMR		(0x003C)
+#define RTL8139_REG_ISR		(0x003E)
+#define RTL8139_REG_TCR		(0x0040)
+#define RTL8139_REG_RCR		(0x0044)
+#define RTL8139_REG_TCTR	(0x0048)
+#define RTL8139_REG_MPC		(0x004C)
+#define RTL8139_REG_9346CR	(0x0050)
+#define RTL8139_REG_CONFIG0	(0x0051)
+#define RTL8139_REG_CONFIG1	(0x0052)
+#define RTL8139_REG_TimerInt	(0x0054)
+#define RTL8139_REG_MSR		(0x0058)
+#define RTL8139_REG_CONFIG3	(0x0059)
+#define RTL8139_REG_CONFIG4	(0x005A)
+#define RTL8139_REG_MULINT	(0x005C)
+#define RTL8139_REG_TSAD	(0x0060)
+#define RTL8139_REG_BMCR	(0x0062)
+#define RTL8139_REG_BMSR	(0x0064)
+#define RTL8139_REG_ANAR	(0x0066)
+#define RTL8139_REG_ANLPAR	(0x0068)
+#define RTL8139_REG_ANER	(0x006A)
+#define RTL8139_REG_DIS		(0x006C)
+#define RTL8139_REG_FCSC	(0x006E)
+#define RTL8139_REG_NWAYTR	(0x0070)
+#define RTL8139_REG_REC		(0x0072)
+#define RTL8139_REG_CSCR	(0x0074)
+#define RTL8139_REG_PHY1_PARM	(0x0078)
+#define RTL8139_REG_TW_PARM	(0x007C)
+#define RTL8139_REG_PHY2_PARM	(0x0080)
+#define RTL8139_REG_CRC(n)	(0x0084+n)
+#define RTL8139_REG_Wakeup(n)	(0x008C+n)
+#define RTL8139_REG_LSBCRC(n)	(0x00CC+n)
+#define RTL8139_REG_CONFIG5	(0x00D8)
+#define RTL8139_REG_REC	(0x0064)
+
+
+#define RTL8193_TSD_CRS		(1<<31)
+#define RTL8193_TSD_TABT	(1<<30)
+#define RTL8193_TSD_OWC		(1<<29)
+#define RTL8193_TSD_CDH		(1<<28)
+#define RTL8193_TSD_NCC_R(v)	((v>>24)&0xF)
+#define RTL8193_TSD_ERTXTH_R(v)	((v>>16)&0x1F)
+#define RTL8193_TSD_ERTXTH_W(v)	((v<<16)&0x1F0000)
+#define RTL8193_TSD_TOK		(1<<15)
+#define RTL8193_TSD_TUN		(1<<14)
+#define RTL8193_TSD_OWN		(1<<13)
+#define RTL8193_TSD_SIZE_R(v)	(v&0x1FFF)
+#define RTL8193_TSD_SIZE_W(v)	(v&0x1FFF)
+
+#endif
